95624f68734914bb849f946b967d706d79944369
AJ Fuhler <aart.fuhler@gmail.com>
2025-07-18 14:18:50 -0500

    added lesson5 assignment12 problem7

---
+++ lesson5/assignment12/problem7.js
@@ -0,0 +1,10 @@
+let ninjaA = (function() {
+  function Ninja(){};
+  return new Ninja();
+})();
+
+// create a ninjaB object
+let ninjaB = {};
+Object.setPrototypeOf(ninjaB, Object.getPrototypeOf(ninjaA));
+
+console.log(ninjaB.constructor === ninjaA.constructor);    // should log true
