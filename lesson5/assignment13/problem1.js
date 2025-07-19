c52c4e361510af3a041c0b894c8c03c1655aabc3
AJ Fuhler <aart.fuhler@gmail.com>
2025-07-19 14:42:49 -0500

    added lesson5 assignment13 problem1

---
+++ lesson5/assignment13/problem1.js
@@ -0,0 +1,25 @@
+let shape = {
+  getType() {
+    return this.type;
+  }
+};
+
+let Triangle = function(a, b, c) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
+  this.type = 'triangle';
+};
+
+Triangle.prototype = shape;
+Triangle.prototype.constructor = Triangle;
+
+shape.getPerimeter = function() {
+  return this.a + this.b + this.c;
+};
+
+let t = new Triangle(3, 4, 5);
+console.log(t.constructor);                 // Triangle(a, b, c)
+console.log(shape.isPrototypeOf(t));        // true
+console.log(t.getPerimeter());              // 12
+console.log(t.getType());                   // "triangle"
