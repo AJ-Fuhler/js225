21d95cf1e42f8663462724ae254fdc09f75285fc
AJ Fuhler <aart.fuhler@gmail.com>
2025-07-19 14:43:16 -0500

    added lesson5 assignment13 problem5

---
+++ lesson5/assignment13/problem5.js
@@ -0,0 +1,18 @@
+let foo = {
+  a: 1,
+  begetObject() {
+    let F = function() {};
+    F.prototype = this;
+    return new F();
+  }
+};
+
+
+Object.prototype.begetObject = function() {
+  let F = function() {};
+  F.prototype = this;
+  return new F();
+}
+
+let bar = foo.begetObject();
+console.log(foo.isPrototypeOf(bar));         // true
