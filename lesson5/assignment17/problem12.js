813c724eb9b41236d3f43095f196c1cb0a4250e0
AJ Fuhler <aart.fuhler@gmail.com>
2025-07-19 14:45:22 -0500

    added lesson5/assignment17 problem12

---
+++ lesson5/assignment17/problem12.js
@@ -0,0 +1,15 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greeting() {
+    return `Hello, I'm ${this.name}. It's very nice to meet you.`;
+  }
+}
+
+class Shouter extends Person {
+  greeting() {
+    return super.greeting().toUpperCase();
+  }
+}
