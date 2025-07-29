a4b2ccd3ff4185ab36bcec239df0852042b31606
AJ Fuhler <aart.fuhler@gmail.com>
2025-07-29 17:11:39 -0500

    made changes to test.js

--- test.js
+++ test.js
@@ -1,14 +1,26 @@
-function Dog(name, breed, weight) {
-  Object.setPrototypeOf(this, Dog.myPrototype);
-  this.name = name;
-  this.breed = breed;
-  this.weight = weight;
+let rotateArray = function(arr) {
+  if (!Array.isArray(arr)) return undefined;
+  
+  return arr.slice(1).concat(arr[0]);
 }
 
-Dog.myPrototype = {
-  bark() {
-    console.log(this.weight > 20 ? 'Woof!' : 'Yip!');
-  }
-};
+let rotateRightmostDigits = function(digits, rotationAmount) {
+  let digitsArr = [...String(digits)];
+  digitsArr.push(digitsArr.splice(-rotationAmount, 1));
+  return Number(digitsArr.join(''));
+}
 
-let maxi = new Dog('Maxi', 'German Shepard', 32);
+let maxRotation = function(number) {
+  let length = String(number).length;
+  for (let rotationAmount = length; rotationAmount >= 2; rotationAmount -= 1) {
+    number = rotateRightmostDigits(number, rotationAmount);
+  }
+
+  return number;
+}
+
+maxRotation(735291);          // 321579
+maxRotation(3);               // 3
+maxRotation(35);              // 53
+maxRotation(105);             // 15 -- the leading zero gets dropped
+maxRotation(8703529146);      // 7321609845
